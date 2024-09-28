a88873e48783a13545fe3a8e84e397620e03ea12
Zain-Zameer <officialmuhammadzain45gmail.com>
2024-09-28 13:05:25 -0700

    project;;;

---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let batteryData = {
+    batteryLevel: 0,
+    charging: false
+};
+
+// Endpoint to update battery status
+app.post('/update-battery', (req, res) => {
+    batteryData.batteryLevel = req.body.batteryLevel;
+    batteryData.charging = req.body.charging;
+    res.sendStatus(200);
+});
+
+// Endpoint to get battery status
+app.get('/get-battery-status', (req, res) => {
+    res.json(batteryData);
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
